2a673cae2ea4e436869a0e5a8f9268fa49b61d57
MiladKetabGhale <ketabii.math@gmail.com>
2018-12-24 09:48:37 +1100

    removing garbage

--- ActSTV/ACTstv.v.d
+++
@@ -1,1 +0,0 @@
-ActSTV/ACTstv.vo ActSTV/ACTstv.glob ActSTV/ACTstv.v.beautified: ActSTV/ACTstv.v ./Parameters.vo ./FrameBase.vo ActSTV/Instantiation.vo
--- ActSTV/Instantiation.v.d
+++
@@ -1,1 +0,0 @@
-ActSTV/Instantiation.vo ActSTV/Instantiation.glob ActSTV/Instantiation.v.beautified: ActSTV/Instantiation.v ./Parameters.vo ./FrameBase.vo
